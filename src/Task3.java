5216d58b0aecee58ee7d8e2c03ca5ffeea8e4882
sergkonhar <sergkonhar@gmail.com>
2018-09-23 17:46:24 +0300

    Update on task3

--- src/Task3.java
+++ src/Task3.java
@@ -1,2 +1,11 @@
+import java.util.Arrays;
 public class Task3 {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4, 5};
+        System.out.println(Arrays.toString(array));
+        for (int i = 0; i < array.length ; i++) {
+            System.out.println(array[i]);
+        }
+
+    }
 }
